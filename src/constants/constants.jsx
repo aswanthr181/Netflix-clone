448c20cb4cb9fd08d580306a91d80a7ffeb50b20
aswanth <aswanthr18tech@gmail.com>
2024-01-24 16:11:27 +0530

    dotenv

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,5 +1,5 @@
 export const baseUrl='https://api.themoviedb.org/3';
 
-// export const API_KEY= '384276909ccf231813b68ed260991ae6' ;
-export const API_KEY= import.meta.env.VITE_REACT_APP_apiKey
+export const API_KEY= '384276909ccf231813b68ed260991ae6' ;
+// export const API_KEY= import.meta.env.VITE_REACT_APP_apiKey
 export const imageUrl='https://image.tmdb.org/t/p/original'
